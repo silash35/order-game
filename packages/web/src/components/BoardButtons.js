e1fb6a36ceefc7e50c2e596d7fba5977bba72947
Silas Henrique <silash35@gmail.com>
2021-01-05 15:59:07 -0300

    start to develop logic

--- packages/web/src/components/BoardButtons.js
+++ packages/web/src/components/BoardButtons.js
@@ -2,7 +2,21 @@ import React from "react";
 import style from "./BoardButtons.module.scss";
 
 const BoardButtons = (props) => {
-  return <button className={style.button}>{props.number}</button>;
+  const [status, setStatus] = React.useState("none"); // loading, none, right or wrong
+
+  const handleClick = () => {
+    console.log(props.number);
+    setStatus("right");
+  };
+
+  return (
+    <button
+      onClick={handleClick}
+      className={style.button + " " + style[status]}
+    >
+      {props.number}
+    </button>
+  );
 };
 
 export default BoardButtons;
