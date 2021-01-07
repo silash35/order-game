ce2125c94751965e42f6c1f3309149133a2e69a6
Silas Henrique <silash35@gmail.com>
2021-01-07 20:45:57 -0300

    add start button

--- packages/web/src/App.js
+++ packages/web/src/App.js
@@ -2,13 +2,26 @@ import React from "react";
 import BoardButtons from "./components/BoardButtons";
 
 const App = () => {
-  const seq = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-  fetch("/api/order").body;
-  const numbers = [];
-  seq.forEach((n) => {
-    numbers.push(<BoardButtons number={n} />);
-  });
-  return <article className="board">{numbers}</article>;
+  const [seq, setSeq] = React.useState(false);
+  const buttons = [];
+
+  const getSeq = () => {
+    fetch("/api/order")
+      .then((res) => res.json())
+      .then((res) => {
+        setSeq(res.randomSequence);
+      });
+  };
+
+  if (seq === false) {
+    buttons.push(<button onClick={getSeq}>Start Game</button>);
+  } else {
+    seq.forEach((n) => {
+      buttons.push(<BoardButtons number={n} />);
+    });
+  }
+
+  return <article className="board">{buttons}</article>;
 };
 
 export default App;
