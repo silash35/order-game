55db3e13f8ac94b250ac641cf738f9ea3b430ad7
Silas Henrique <silash35@gmail.com>
2021-01-08 13:45:40 -0300

    add new font

--- packages/web/src/index.js
+++ packages/web/src/index.js
@@ -2,6 +2,6 @@ import { render } from "react-dom";
 import React from "react";
 import App from "./App";
 
-import "./style.scss";
+import "./globals.scss";
 
 render(<App />, document.querySelector("#root"));
