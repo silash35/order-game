ae6403a27023a45fa645c3d80925b5017d445245
Silas Henrique <silash35@gmail.com>
2021-01-08 14:56:17 -0300

    order is checked on server side

--- packages/web/src/components/Board.js
+++ packages/web/src/components/Board.js
@@ -5,13 +5,20 @@ import style from "./Board.module.scss";
 
 const Board = () => {
   const [seq, setSeq] = React.useState(false);
+  const [userSeq, setUserSeq] = React.useState([]);
   const buttons = [];
 
   if (seq === false) {
     buttons.push(<StartGame setSeq={setSeq} />);
   } else {
     seq.forEach((n) => {
-      buttons.push(<BoardButtons number={n} />);
+      buttons.push(
+        <BoardButtons
+          number={n}
+          userSeq={{ value: userSeq, set: setUserSeq }}
+          setSeq={setSeq}
+        />
+      );
     });
   }
 
--- packages/web/src/components/BoardButtons.js
+++ packages/web/src/components/BoardButtons.js
@@ -4,15 +4,26 @@ import style from "./BoardButtons.module.scss";
 const BoardButtons = (props) => {
   const [status, setStatus] = React.useState("none"); // loading, none, right or wrong
 
-  fetch("/api/order", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: "[8,2,4,5,6]",
-  });
-
   const handleClick = () => {
-    console.log(props.number);
-    setStatus("right");
+    setStatus("loading");
+
+    props.userSeq.value = [].concat(props.userSeq.value, [props.number]);
+    fetch("/api/order", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(props.userSeq.value),
+    })
+      .then((res) => res.json())
+      .then(({ isOrdered }) => {
+        if (isOrdered) {
+          setStatus("right");
+        } else {
+          setStatus("wrong");
+          props.setSeq(false);
+        }
+      });
+
+    props.userSeq.set(props.userSeq.value);
   };
 
   return (
