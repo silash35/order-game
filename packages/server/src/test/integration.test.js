f8fbad2831097b64997353c95d3681aeb5d08c30
Silas Henrique <silash35@gmail.com>
2021-01-23 11:22:53 -0300

    add test for backend server

--- packages/server/src/test/integration.test.js
+++ packages/server/src/test/integration.test.js
@@ -1,5 +1,34 @@
-describe("Math Operations", () => {
-  it("should sum", () => {
-    expect(1 + 1).toBe(2);
+/**
+ * @jest-environment jsdom
+ */
+
+import { io } from "socket.io-client";
+
+import http from "../app";
+
+const PORT = 9000;
+
+let socket;
+
+beforeAll(() => {
+  http.listen(PORT, () => {
+    console.log(`App listening at http://localhost:${PORT}`);
+  });
+  socket = io(`ws://localhost:${PORT}`);
+});
+
+afterAll((done) => {
+  socket.close();
+  http.close(() => done());
+});
+
+describe("Socket.io connections", () => {
+  it("should give a sequence", (done) => {
+    socket.on("sequence", ({ seq }) => {
+      expect(1 + 1).toBe(2);
+      done();
+    });
+
+    socket.emit("game started");
   });
 });
