fda6994c168df4b65b9f3aeb234f5f27d3947835
Silas Henrique <silash35@gmail.com>
2021-01-27 14:25:49 -0300

    jest now is executed on each package

--- packages/web/jest.config.js
+++ packages/web/jest.config.js
@@ -3,4 +3,9 @@ const { name } = require("./package.json");
 module.exports = {
   displayName: name,
   name,
+  testEnvironment: "jest-environment-jsdom",
+  verbose: true,
+  silent: true,
+  clearMocks: true,
+  transform: {},
 };
