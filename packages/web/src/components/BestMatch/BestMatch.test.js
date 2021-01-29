b2cdad714097e75cec36eecf5eeddab8e8ae6620
Silas Henrique <silash35@gmail.com>
2021-01-29 20:49:58 -0300

    testing react

--- packages/web/src/components/BestMatch/BestMatch.test.js
+++ packages/web/src/components/BestMatch/BestMatch.test.js
@@ -2,16 +2,22 @@ import { jest } from "@jest/globals";
 import { render } from "@testing-library/react";
 import React from "react";
 
+// mock socket.io
 const socket = {};
-jest.mock("./BestMatch.module.scss");
 socket.on = jest.fn(() => {});
+global.socket = socket;
 
 import BestMatch from "./index";
 
-describe("", () => {
-  it("", () => {
-    global.socket = socket;
+describe("BestMatch react component", () => {
+  it("should be null on beginning", () => {
     const { queryByText } = render(<BestMatch />);
-    expect(queryByText("milliseconds")).not.toBeTruthy();
+    expect(queryByText("milliseconds")).toBeNull();
+  });
+  it("should be null on beginning", () => {
+    const { queryByText } = render(<BestMatch />);
+
+    console.log(socket.on.mock.calls[0]);
+    expect(1).not.toBe(2);
   });
 });
