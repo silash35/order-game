2d579a42cf67712e9b89274709aef8974bbd358b
Silas Henrique <silash35@gmail.com>
2021-02-08 15:40:23 -0300

    add cypress to github workflow

--- cypress/integration/e2e.spec.js
+++ cypress/integration/e2e.spec.js
@@ -1,5 +1,26 @@
-describe("My First Test", () => {
-  it("Does not do much!", () => {
-    expect(true).to.equal(true);
+const options = { matchCase: false };
+
+describe("Play the Game", () => {
+  it("should lose!", () => {
+    cy.visit("/");
+    cy.contains("start", options).click();
+
+    cy.contains(/^1$/).click();
+    cy.contains(/^2$/).click();
+    cy.contains(/^6$/).click();
+
+    cy.contains("best time", options).should("not.exist");
+    cy.contains("lose", options);
+  });
+
+  it("should Win!", () => {
+    cy.visit("/");
+    cy.contains("start", options).click();
+
+    for (let i = 1; i <= 10; i++) {
+      cy.contains(new RegExp(`^${i}$`)).click();
+    }
+
+    cy.contains("win", options);
   });
 });
